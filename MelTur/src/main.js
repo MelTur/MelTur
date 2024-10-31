ff336e2c29f7a3d7bccb1b16df4e088424128f21
Q4ck <quckpriv@hotmail.com>
2024-10-31 00:26:50 -0500

    Subiendo archivos

--- MelTur/src/main.js
+++ MelTur/src/main.js
@@ -1,6 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
